890b4169cc524db2921acbb04f2ae22fb437a785
Ege Uysal <hi@egeuysal.com>
2026-06-28 12:49:08 -0500

    fix(ui): use dark browser theme color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   themeColor: [
     { media: "(prefers-color-scheme: light)", color: "#f8f8f8" },
-    { media: "(prefers-color-scheme: dark)", color: "#f8f8f8" },
+    { media: "(prefers-color-scheme: dark)", color: "#000000" },
   ],
 };
 
