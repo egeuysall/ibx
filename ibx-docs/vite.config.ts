f1ac94a1d3da466aa3fe696e551821aff98dae9a
Ege Uysal <hi@egeuysal.com>
2026-08-21 22:25:06 -0500

    fix: bundle docs scheduler dependency

--- ibx-docs/vite.config.ts
+++ ibx-docs/vite.config.ts
@@ -4,5 +4,6 @@ import { holocron } from '@holocron.so/vite'
 export default defineConfig({
   clearScreen: false,
   base: '/docs/',
+  ssr: { noExternal: ['scheduler'] },
   plugins: [holocron({ pagesDir: './src' })],
 })
